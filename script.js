2a34e4c2158c2bda637f444961f193d3a891af5c
LukePadawalker <padurariu.luca@gmail.com>
2024-01-09 21:48:22 +0100

    created the second exercise

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-console.log("js ok");
+// console.log("js ok");
 
-// Palindroma exercise
+// // Palindroma exercise
 
-const userWord = prompt("Inserisci una parola", "otto");
+// const userWord = prompt("Inserisci una parola", "otto");
 
-function isPalindromaWord(word){
+// function isPalindromaWord(word){
 
-    let splitWord = word.split("");
-    console.log(splitWord);
+//     let splitWord = word.split("");
+//     console.log(splitWord);
 
-    let reversedWord = splitWord.slice().reverse();
-    console.log(reversedWord);
+//     let reversedWord = splitWord.slice().reverse();
+//     console.log(reversedWord);
 
-    let result = splitWord.join('') === reversedWord.join('') ? true : false;
+//     let result = splitWord.join('') === reversedWord.join('') ? true : false;
 
-    return result;
-}
+//     return result;
+// }
 
-console.log(isPalindromaWord(userWord));
+// console.log(isPalindromaWord(userWord));
 
 
 // Odd & Even exercise
@@ -35,13 +35,62 @@ console.log(isPalindromaWord(userWord));
 */
 
 
-const OddOrEven = prompt("Pari o Dispari?", "Pari")
+const userChoice = prompt("Pari o Dispari?", "Pari");
+const userNum = prompt("Inserisci un numero da 1 a 5", "3");
 
-let OddOrEvenValidator = OddOrEven === "pari" && OddOrEven === "dispari" ? true : false;
+if (userChoice !== "Pari" && userChoice !== "Dispari"){
+    console.log("Hai sbagliato. Scegli se pari o dispari");
+} else if (isNaN(userNum) || userNum < 1 || userNum > 5) {
+    console.log("Hai inserito un numero non valido. Inserisci un numero da 1 a 5.");
+} else {
+    console.log("Input valido.");
 
-if (OddOrEvenValidator = false) {
-    console.log("il numero è sbagliato")
+    function getRandomNumber(num){
+
+        let randomNum = Math.floor(Math.random() * 5) + 1;
+        console.log("RandomNum = " + randomNum);
+
+
+        let sum = randomNum + num;
+        console.log("Sum = " + sum);
+
+        return sum;
+
+    }
+
+    function isEven(){
+
+        if (getRandomNumber(userNum) % 2 == 0){
+            console.log("IL numero è pari");
+        } else{
+            console.log("IL numero è dispari");
+        }
+    }
+
+    isEven();
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+// let OddOrEvenValidator = userChoice == "pari" && userChoice == "dispari" ? true : false;
+
+// console.log(OddOrEvenValidator);
+
+
+// if (OddOrEvenValidator = false) {
+//     console.log("il numero è sbagliato");
+    
+// }
 
 
 
