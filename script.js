560999b4ce55e338e46359925b559810e50aa5ff
LukePadawalker <padurariu.luca@gmail.com>
2024-01-09 15:57:05 +0100

    created the first sketch

--- script.js
+++ script.js
@@ -2,9 +2,21 @@ console.log("js ok");
 
 // Palindroma exercise
 
-const userWord = prompt("Inserisci una parola", "Giorgio");
+const userWord = prompt("Inserisci una parola", "otto");
 
+function isPalindromaWord(word){
 
+    let wordContainer = word;
+    let reversedWord = wordContainer.reverse();
+
+    if (wordContainer = reversedWord){
+        return true;
+    }else{
+        return false
+    }
+}
+
+console.log(isPalindromaWord(userWord));
 
 
 // Odd & Even exercise
