0cdeb86b6ac41fd715f634d2c72f0becca691ef1
LukePadawalker <padurariu.luca@gmail.com>
2024-01-09 22:09:18 +0100

    final minor changes

--- script.js
+++ script.js
@@ -1,32 +1,34 @@
-// console.log("js ok");
+console.log("js ok");
 
-// // Palindroma exercise
+// Palindroma exercise
 
-// const userWord = prompt("Inserisci una parola", "otto");
+const userWord = prompt("Inserisci una parola", "otto");
 
-// function isPalindromaWord(word){
+function isPalindromaWord(word){
 
-//     let splitWord = word.split("");
-//     console.log(splitWord);
+    let splitWord = word.split("");
+    console.log(splitWord);
 
-//     let reversedWord = splitWord.slice().reverse();
-//     console.log(reversedWord);
+    let reversedWord = splitWord.slice().reverse();
+    console.log(reversedWord);
 
-//     let result = splitWord.join('') === reversedWord.join('') ? true : false;
+    let result = splitWord.join('') === reversedWord.join('') ? true : false;
 
-//     return result;
-// }
+    return result;
+}
 
-// console.log(isPalindromaWord(userWord));
+console.log(isPalindromaWord(userWord));
 
 
 // Odd & Even exercise
 
 
-// Passaggi
 
 
 /*
+
+Passaggi
+
  - raccogliamo le informazioni dall'utente
  - generiamo un numero con una funzione
  - sommiamo i 2 numeri
